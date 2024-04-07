085862222410f8d79e5c5e747741be98056dd7d8
Wurpa22 <hyeyun.0504@gmail.com>
2024-04-07 16:48:51 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,13 @@
+//11382: ²¿¸¶ Á¤¹Î
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	long long a, b, c;
+	long long sum = 0;
+
+	scanf("%lld %lld %lld", &a, &b, &c);
+	sum = a + b + c;
+	printf("%lld", sum);
+}
